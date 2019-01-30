279fd1893404a8f92bcf550df59e48e70e19c155
CXRyy <44918146+CXRyy@users.noreply.github.com>
2019-01-30 09:51:03 +0800

    Update table.js

--- routes/admin/table.js
+++ routes/admin/table.js
@@ -2,7 +2,7 @@ const express=require("express");
 const router=express.Router();
 const pool=require("../../pool.js");
 router.get("/",(req,res)=>{
-    var sql="SELECT * FROM xfn_table";
+    var sql="SELECT * FROM xfn_table ORDER BY tid";
     pool.query(sql,(err,result)=>{
         if(err){
             throw err;
